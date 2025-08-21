218ac37960a7218e46e95f2cb8eea7a0d8bee174
Will5l <will.malloch@ucas-edu.net>
2025-08-21 10:09:48 -0600

    seasons starter

--- notes_2nd/thing.py
+++ notes_2nd/thing.py
@@ -1,4 +1,9 @@
 num = int(input("Give me a number: "))
 num2 = int(input("Give me a number: "))
-choice = input("Would you like the numbers to be (1)added, or (2) multipled")
-print(num+num2)
+choice = int(input("Would you like the numbers to be (1)added, or (2) multipled: "))
+if choice==1:
+    print(num+num2)
+elif choice==2:
+    print(num*num2)
+else:
+    print("Invalid option")
---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+
+
+last_name = input("What's your last name?: ")
+season = input("What's your favorite season: ")
+print("Wow", last_name, "san, I like", season, "too!")
