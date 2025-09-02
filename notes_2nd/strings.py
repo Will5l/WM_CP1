999afbddf82d05ec3b90417b3206c34a5c7d8898
Will5l <will.malloch@ucas-edu.net>
2025-09-02 10:50:57 -0600

    notes

--- notes_2nd/strings.py
+++ notes_2nd/strings.py
@@ -1,2 +1,38 @@
 # WM 2nd Strings Notes
 
+#Examples
+sentence = "The quick brown fox jumps over the lazy dog!"
+first_name = 'Xavier'
+month = 'September'
+food = 'chocolate'
+last_name = "LaRose"
+
+
+# If i need to use ' or "
+name = "La'Rose"
+best = '"The Return of the King is best book ever!"'
+
+funny = "The dog jumped into the lake"
+
+
+# Length of strings
+
+length = len(month)
+print("September has", length, "letters")
+
+
+
+# Escape Character
+print('\tWill said \n"Don\'t die"')
+
+
+# Concatenate
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+#Index
+
+
+# Slice
+print(last_name[2:6])
