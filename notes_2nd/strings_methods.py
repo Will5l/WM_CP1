5b8bb7e900b3bdf273ebb4749067ad7a02b722b8
Will5l <will.malloch@ucas-edu.net>
2025-09-04 10:20:31 -0600

    notes

---
+++ notes_2nd/strings_methods.py
@@ -0,0 +1,13 @@
+# WM 2nd string methods notes
+
+name = input("What is your name: ").strip().lower().title()
+# .lower() => make it all lower case
+# .upper() => all upper case
+# .capitalize() => capitalizes the first letter.
+# .title() => capitalizes the first letter of every word.
+
+print("Hello "+ name +", it is nice to meet you!")
+
+age = input("Bro how old are you?")
+
+Print)
---
+++ practice/madlib.py
@@ -0,0 +1,7 @@
+# WM 2nd Madlib
+
+noun_one = 
+noun_two = 
+verb_one = 
+verb_two = 
+adjective = 
