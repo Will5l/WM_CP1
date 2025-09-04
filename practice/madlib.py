f658bbc20e01441be8b68eef137caa69a3d71933
Will5l <will.malloch@ucas-edu.net>
2025-09-04 10:49:54 -0600

    madlib

--- practice/madlib.py
+++ practice/madlib.py
@@ -6,4 +6,4 @@ verb_one = input("Give me a verb:")
 verb_two = input("Give me another verb: ")
 adjective = input("Give me an adjective: ")
 
-print(noun_one+" did "+verb_one+", and this shocked "+noun_two+", who couldn't believe that "+noun_one+" was able to "+verb_one+" so "+adjective+" and so "+noun_two+" did "+verb_two+".")
+print(noun_one+" did "+verb_one+", and this shocked "+noun_two+". They couldn't believe that "+noun_one+" was able to "+verb_one+" so "+adjective+". Due to this, "+noun_two+" did "+verb_two+".")
