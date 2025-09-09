a21c7a6a6100f9eece6eee42974e7d4ba2ea51c1
Will5l <will.malloch@ucas-edu.net>
2025-09-09 10:11:52 -0600

    thing

--- practice/idiot_proof.py
+++ practice/idiot_proof.py
@@ -1,9 +1,8 @@
 # WM 2nd Idiot Proof
 
 full_name = input("What is your full name? ").title()
-phone_num = input("What is your phone number? ")
+phone_num = str(input("What is your phone number? "))
 GPA = input("What is your GPA? ")
-if phone_num.index[3] == "-" and phone_num.index[7] == "-":
-    print(f"Name: {full_name}\nPhone: {phone_num}\nGPA: {GPA:.1}")
-elif phone_num.index[3] =="-" and phone_num.index[7] == " ":
-    phone_num = phone_num.replace[7, "-"]
+phone_num = (phone_num.replace[3:"-"])
+phone_num = (phone_num.replace[7:"-"])
+print(f"Name: {full_name}\nPhone: {phone_num}\nGPA: {GPA:.1}")
