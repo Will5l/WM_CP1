7018f575f3deb2dfb68d8e742fefcafe0eeffa5f
Will5l <will.malloch@ucas-edu.net>
2025-09-09 10:52:18 -0600

    basic calc

---
+++ practice/basic_calc.py
@@ -0,0 +1,27 @@
+num1 = input("Give me a number")
+num2 = input("Give me another number")
+equation_type = ("Choose an number for the arithmetic method.\n1. additon\n2. subtraction\n3. intger divison\n4. multiplication\n5. expontent \n6. modular divison\n7. floor divison")
+while equation_type <= 8:
+    if equation_type == 1:
+        additon_product = (num1+num2)
+        print(f"{num1}+{num2}={additon_product}")
+    elif equation_type == 1:
+        additon_product = (num1+num2)
+        print(f"{num1}+{num2}={additon_product}")
+    elif equation_type == 1:
+        additon_product = (num1+num2)
+        print(f"{num1}+{num2}={additon_product}")
+    elif equation_type == 1:
+        additon_product = (num1+num2)
+        print(f"{num1}+{num2}={additon_product}")
+    elif equation_type == 1:
+        additon_product = (num1+num2)
+        print(f"{num1}+{num2}={additon_product}")
+    elif equation_type == 1:
+        additon_product = (num1+num2)
+        print(f"{num1}+{num2}={additon_product}")
+    elif equation_type == 1:
+        additon_product = (num1+num2)
+        print(f"{num1}+{num2}={additon_product}")
+    else:
+        print()
