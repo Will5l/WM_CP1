a5940096ad1afd011a443a33d0512df6f4b2a46d
Will5l <will.malloch@ucas-edu.net>
2025-09-09 10:39:42 -0600

    idiot proof

--- practice/idiot_proof.py
+++ practice/idiot_proof.py
@@ -2,7 +2,9 @@
 
 full_name = input("What is your full name? ").title()
 phone_num = str(input("What is your phone number? "))
-GPA = input("What is your GPA? ")
-phone_num = (phone_num.replace[3:"-"])
-phone_num = (phone_num.replace[7:"-"])
-print(f"Name: {full_name}\nPhone: {phone_num}\nGPA: {GPA:.1}")
+third = phone_num[3]
+seven = phone_num[7]
+GPA = float(input("What is your GPA? "))
+phone_num = (phone_num.replace(third, "-"))
+phone_num = (phone_num.replace(seven, "-"))
+print(f"Name: {full_name}\nPhone: {phone_num}\nGPA: {GPA:.1f}")
