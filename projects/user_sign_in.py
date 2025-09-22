d90de412861f7370819ada664061886093a49f12
Will5l <will.malloch@ucas-edu.net>
2025-09-22 10:50:59 -0600

    user

--- projects/user_sign_in.py
+++ projects/user_sign_in.py
@@ -1,8 +1,11 @@
 # WM 2nd User Sign in
+
 username_input = input("Enter Username(case sensitive): ")
 password_input = input("Enter Password(case sensitive): ")
+# Credintials
 username = "OneRing"
 password = "RuleThemAll"
+# checking
 if username_input == username:
     if password_input == password:
         print(f"Welcome, {username_input}")
