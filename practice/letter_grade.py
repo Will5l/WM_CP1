aefd1e09da81b46a44deece2e0da6ade9f46623c
Will5l <will.malloch@ucas-edu.net>
2025-09-24 10:52:21 -0600

    letter

--- practice/letter_grade.py
+++ practice/letter_grade.py
@@ -1,20 +1,20 @@
 # WM 2nd Letter Grade
 grade = 1
-while grade != 0:
-    grade = float(input("Give me your grade percentage and I'll give you your letter grade. Type 0 if you want to end."))
-    if grade >= 94:
-        print(f"You have {grade:.2f}%, an A")
-    elif grade >= 90:
-        print(f"You have {grade:.2f}%, an A-")
-    elif grade >= 87:
-        print(f"You have {grade:.2f}%, a B+")
-    elif grade >= 83:
-        print(f"You have {grade:.2f}%, a B")
-    elif grade >= 80:
-        print(f"You have {grade:.2f}%, a B-")
-    elif grade >= 77:
-        print(f"You have {grade:.2f}%, a C+")
-    elif grade >= 74:
+
+grade = float(input("Give me your grade percentage and I'll give you your letter grade."))
+if grade >= 94:
+    print(f"You have {grade:.2f}%, an A")
+elif grade >= 90:
+    print(f"You have {grade:.2f}%, an A-")
+elif grade >= 87:
+    print(f"You have {grade:.2f}%, a B+")
+elif grade >= 83:
+    print(f"You have {grade:.2f}%, a B")
+elif grade >= 80:
+    print(f"You have {grade:.2f}%, a B-")
+elif grade >= 77:
+    print(f"You have {grade:.2f}%, a C+")
+elif grade >= 74:
         print(f"You have {grade:.2f}%, a C")
     elif grade >= 70:
         print(f"You have {grade:.2f}%, a C-")
