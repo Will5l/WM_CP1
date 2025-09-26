5f1641213e84d2017a49d936c0b8ccb3f69cbef3
Will5l <will.malloch@ucas-edu.net>
2025-09-26 10:51:11 -0600

    fix

---
+++ notes_2nd/debugging.py
@@ -0,0 +1,9 @@
+# WM 2nd Debugging
+
+# Runtime errors - the code will start running and it will not finish
+import random
+
+while True:
+    denominator = random.randint(0,5)
+
+    print(10/denominator)
---
+++ practice/fix_game.py
@@ -0,0 +1,28 @@
+# WM 2nd Fix Game
+
+import random
+def start_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    number_to_guess = random.randint(1, 100)
+    max_attempts = 10
+    attempts = 0
+    game_over = False
+    while not game_over:
+        # Was not ticking the attempt counter. you had infinte attempts. logic
+        attempts += 1
+        #Guess needs to be an int. Runtime error
+        guess = int(input("Enter your guess: "))
+        if attempts >= max_attempts:
+            print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
+            game_over = True
+        # elif. you dont want it to run if the above runs. runtime
+        elif guess == number_to_guess:
+            print("Congratulations! You've guessed the number!")
+            game_over = True
+        elif guess > number_to_guess:
+            print("Too high! Try again.")
+        elif guess < number_to_guess:
+            print("Too low! Try again.")  #Continue was not needed. logic
+    print("Game Over. Thanks for playing!")
+start_game()
