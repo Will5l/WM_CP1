a678fd1f1eee5cd4e86d29738d6927019a8b02b2
Will5l <will.malloch@ucas-edu.net>
2025-09-29 10:44:11 -0600

    list notes

---
+++ notes_2nd/lists.py
@@ -0,0 +1,32 @@
+# WM 2nd List notes
+
+import random
+names = ["Alex", "Katie", "Cora", "Andrew", "Jake", "Eric", 5, 3.14, False]
+
+print(names)
+print(names[3])
+print([random.randint(1,len(names))])
+print(random.choice(names))
+names[-1] = "Xavier"
+names.extend(["Treyson", "Tia"])
+names += ["Joseph", "Israel", "Zee"]
+names.remove(3.14)
+thing = names.index(5)
+names.insert(3, "Vienna")
+names.pop(thing)
+print(names)
+
+board = [[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+
+board[1][1] = "X"
+
+print(board)
+# List (changable, ordered)
+# Tuple (Not changable, ordered)
+classes = ("Bard", "Monk", "Barbarian", "Paladin")
+
+# Set (changable, unordered)
+fruit = {"Apple", "Orange", "Straberry", "Kiwi"}
+print(fruit)
