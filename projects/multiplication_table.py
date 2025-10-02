f29b7e73fcea1339aa859bad6e06faffe11817c1
Will5l <will.malloch@ucas-edu.net>
2025-10-02 10:30:39 -0600

    multi table done

--- projects/multiplication_table.py
+++ projects/multiplication_table.py
@@ -5,20 +5,29 @@ nums = [1,2,3,4,5,6,7,8,9,10,11,12]
 table = [
     [],
     [],
-    []
-]
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],]
 for num in nums:
-    table[thing][0] = (num*1)
-    table[thing][1] = (num*2)
-    table[thing][1] = (num*3)
-    table[thing][1] = (num*4)
-    table[thing][1] = (num*5)
-    table[thing][1] = (num*6)
-    table[thing][1] = (num*7)
-    table[thing][1] = (num*8)
-    table[thing][1] = (num*9)
-    table[thing][1] = (num*10)
-    table[thing][1] = (num*11)
-    table[thing][1] = (num*12)
+    table[thing].insert(0, num*1)
+    table[thing].insert(1, num*2)
+    table[thing].insert(2, num*3)
+    table[thing].insert(3, num*4)
+    table[thing].insert(4, num*5)
+    table[thing].insert(5, num*6)
+    table[thing].insert(6, num*7)
+    table[thing].insert(7, num*8)
+    table[thing].insert(8, num*9)
+    table[thing].insert(9, num*10)
+    table[thing].insert(10, num*11)
+    table[thing].insert(11, num*12)
     thing += 1
-print(table)
+for row in table:
+    print(row)
