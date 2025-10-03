1fdd3cc7e267b407aa9eebc9e6089ca51a6a7256
Will5l <Will.malloch@ucas-edu.net>
2025-10-03 18:47:15 +0000

    rock paper scissors

--- projects/rock_paper_scissors.py
+++ projects/rock_paper_scissors.py
@@ -13,40 +13,28 @@ def slow_print(text, delay=0.05):
 slow_print("We are gonna play rock paper scissors")
 
 while True:
-    user = input(slow_print("Choose rock paper or scissors"))
+    user = input("Choose rock paper or scissors (q to leave)")
     computer = random.randint(1,3)
     if user == "rock":
         if computer == 1:
             print("Tie")
             print(
-    "  ___" \
-    " /   |" \
-    " |   /" \
-    "/    |" \
-    "|____|"
+    "  ___\n" \
+    " /   |\n" \
+    " |   /\n" \
+    "/    |\n" \
+    "|____|\n"
 )*2
-
-    elif user == "paper":
-        if computer == 1:
-
-    elif user == "scissor":
-        if
-
-print(
-    "  ___" \
-    " /   |" \
-    " |   /" \
-    "/    |" \
-    "|____|"
+        elif computer == 2:
+            print("You lost")
+            print(
+    "  ___\n" \
+    " /   |\n" \
+    " |   /\n" \
+    "/    |\n" \
+    "|____|\n"
 )
-print(
-    " __"
-    "(__)" \
-    " __   ____" \
-    "(__)/"
-)
-
-print(
+            print(
     " __________\n" \
     "|          |\n" \
     "|          |\n" \
@@ -55,3 +43,112 @@ print(
     "|          |\n" \
     "|__________|\n" \
 )
+        elif computer == 3:
+            print("You win")
+            print(
+    "  ___\n" \
+    " /   |\n" \
+    " |   /\n" \
+    "/    |\n" \
+    "|____|\n"
+)
+            print(
+    " __\n"
+    "(__)_____\n" \
+    " __   ____\n" \
+    "(__)/\n"
+)
+
+    elif user == "paper":
+        if computer == 1:
+            print("You win")
+            print(
+    " __________\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|__________|\n" \
+)
+            print(
+    "  ___\n" \
+    " /   |\n" \
+    " |   /\n" \
+    "/    |\n" \
+    "|____|\n"
+)
+        elif computer == 2:
+            print("Tie")
+            print(
+    " __________\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|__________|\n" \
+)*2
+        elif computer == 3:
+            print("You lost")
+            print(
+    " __________\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|__________|\n" \
+)
+            print(
+    " __\n"
+    "(__)_____\n" \
+    " __   ____\n" \
+    "(__)/\n"
+)
+    elif user == "scissor":
+        if computer == 1:
+            print("You lost")
+            print(
+    " __\n"
+    "(__)_____\n" \
+    " __   ____\n" \
+    "(__)/\n"
+)
+            print(
+    "  ___\n" \
+    " /   |\n" \
+    " |   /\n" \
+    "/    |\n" \
+    "|____|\n"
+)  
+        elif computer == 2:
+            print("You won")
+            print(
+    " __\n"
+    "(__)_____\n" \
+    " __   ____\n" \
+    "(__)/\n"
+)
+            print(
+    " __________\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|          |\n" \
+    "|__________|\n" \
+)
+        elif computer == 3:
+            print("Tie")
+            print(
+    " __\n"
+    "(__)_____\n" \
+    " __   ____\n" \
+    "(__)/\n"
+)*2
+    elif user == "q":
+        break
+    else:
+        print("Invalid")
+print("Session Terminated")
