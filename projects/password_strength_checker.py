8a4771b20d6c8c5a52a52a3bfa9b20666846d336
Will5l <Will.malloch@ucas-edu.net>
2025-10-10 17:48:43 +0000

    password

--- projects/password_strength_checker.py
+++ projects/password_strength_checker.py
@@ -19,22 +19,44 @@ if len(password) >= 8:
 for i in password:
     if i.isupper():
         rating += 1
+        upper = True
 #Check the password for the lowercase, if it has one, add 1 to the rating
 for i in password:
     if i.islower():
         rating += 1
+        lower = True
 
 #Check the password for numbers, if it has any, add 1 to the string
 for i in password:
     if i.isnumeric():
         rating += 1
+        num = True
 
 #Check the password with the special character list, if it has one of them, add 1 to the rating
 for i in password:
     if i in special_chars:
         rating += 1
+        special = True
 #Display the rating of the password, and how strong it is, 1-2 points: Weak 3 points: Moderate 4 points: Strong 5 points: Very Strong
-
+if rating == 1 or rating == 2:
+    print("Your password is weak")
+elif rating == 3:
+    print("Your passwords strength is ok, but could be better")
+elif rating == 4:
+    print("Your password is strong")
+elif rating == 5:
+    print("Your password is very strong")
 #If the password is missing something, display what its missing, and make a suggestion for the corresponding problem.
+if rating < 5:
+    if length == False:
+        print("You should make your password longer")
+    if upper == False:
+        print("You should add some capitals to your password")
+    if lower == False:
+        print("You should add some lower case letters to your password")
+    if num == False:
+        print("You should add a number to your password")
+    if special == False:
+        print("You should add special characters to your password")
 
 #Make a bar that fills in by fifths based on the strength.
