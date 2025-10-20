b57e7bd30ea8d0a31ed21f013ca6435794a6b8cc
Will5l <will.malloch@ucas-edu.net>
2025-10-20 10:50:59 -0600

    functions notes

---
+++ notes_2nd/functions.py
@@ -0,0 +1,13 @@
+# WM 2nd Functions Notes
+num = 0
+def add(x,y):
+    return x+y
+
+while num < add(5,5):
+    print("Duck")
+    num += 1
+print("Goose")
+print(f"Results is: {add(9872143988,129847)}")
+total = add(84190,194801)
+print(add(add(3.14,.85), 10))
+add(42,7)
