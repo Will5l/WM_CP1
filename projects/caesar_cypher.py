1e6c37eaef1d697825e2951f479e65ca27a3f161
Will5l <will.malloch@ucas-edu.net>
2025-10-23 10:50:55 -0600

    thing

--- projects/caesar_cypher.py
+++ projects/caesar_cypher.py
@@ -1,34 +1,7 @@
 # WM 2nd caesar cypher
 
 # Ask for encode or decode
-# Make a list for capital letter
 final_msg = ""
-cap = ["A", "B", "C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-print(cap.index("C"))
-lower = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-# Make a list for lower case letters
-while True:
-    choice = input("Would you like to (1)encode or (2)decode? ")
-    # Ask for the message, and change display based on previous option. loop till a valid choice is given
-
-    if choice == 1:
-        msg = input("What would you like to encode? ")
-        shift = input("What would you like to shift by? ")
-        if shift.isnumeric():
-            w
-        else:
-            print("Shift isn't a number")
-    elif choice == 2:
-        msg = input("What would you like to decode? ")
-        shift = input("What would you like to shift by? ")
-        if shift.isnumeric():
-            w
-        else:
-            print("Shift isn't a number")
-    else:
-        print("Invalid choice")
-# Ask for the integer to encode with
-# Make a function for the encoding and one for decoding
 def encoding(x):
     for i in x:
         if i in cap:
@@ -43,6 +16,48 @@ def encoding(x):
             for i in lower:
                 if lower.index(i) == y:
                     final_msg = final_msg+i
+def decoding(x):
+    for i in x:
+        if i in cap:
+            y = cap.index(i)
+            y = y+shift
+            for i in cap:
+                if cap.index(i) == y:
+                    final_msg = final_msg+i
+        elif i in lower():
+            y = lower.index(i)
+            y = y+shift
+            for i in lower:
+                if lower.index(i) == y:
+                    final_msg = final_msg+i
+        else:
+            final_msg = final_msg+i
+# Make a list for capital letter
+cap = ["A", "B", "C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+lower = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+# Make a list for lower case letters
+while True:
+    choice = input("Would you like to (1)encode or (2)decode? ")
+    # Ask for the message, and change display based on previous option. loop till a valid choice is given
+
+    if choice == "1":
+        msg = input("What would you like to encode? ")
+        shift = int(input("What would you like to shift by? "))
+
+        encoding(msg)
+        print(final_msg)
+        break
+    elif choice == "2":
+        msg = input("What would you like to decode? ")
+        shift = int(input("What would you like to shift by? "))
+        decoding(msg)
+        print(final_msg)
+        break
+    else:
+        print("Invalid choice")
+# Ask for the integer to encode with
+# Make a function for the encoding and one for decoding
+
 # Make it ignore numbers spaces and other special chars. Ignore negatives
 # Use indexes to shift the letters
 # Take the encoded/decoded value, and display it with the respective title
