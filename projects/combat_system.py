86173cc7fb841445b7c8e2464eba63c7fc4fa1b2
Will5l <Will.malloch@ucas-edu.net>
2025-10-24 19:43:47 +0000

    combat system print stats

--- projects/combat_system.py
+++ projects/combat_system.py
@@ -7,8 +7,8 @@ health_potions = 3
 def user_turn():
     print("What do you want to do?")
     if user_class == "Fighter":
-        (f"1. Normal Attack\n 2. Second Wind\n 3. Drink Health Potion({health_potions} left)\n 4. Flee")
-user_class_choice = input("What class are you?\n1 Fighter\n 2 Mage\n 3 Rogue")
+        return input(f"1. Normal Attack\n 2. Second Wind\n 3. Drink Health Potion({health_potions} left)\n 4. Flee")
+user_class_choice = input("What class are you?\n1 Fighter\n 2 Mage\n 3 Rogue\n")
 if user_class_choice == "1":
     health = 30
     defense = 16
@@ -30,3 +30,13 @@ elif user_class_choice == "3":
     str_mod = 3
     int_mod = 2
     user_class = "Rogue"
+print(f"Your a {user_class}, you have:\n{health} hp\n{defense} AC\n+{dex_mod} dex mod\n+{str_mod} str mod\n+{int_mod} int mod")
+
+mon_health = 0
+while health > 0 and mon_health > 0:
+    if user_int >= mon_int:
+        user_turn
+        mon_turn
+    elif user_int < mon_int:
+        mon_turn
+        user_turn
