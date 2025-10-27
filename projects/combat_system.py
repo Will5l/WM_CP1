f9a8abc05529dd13726319fe9761abaa67fc3f9b
Will5l <Will.malloch@ucas-edu.net>
2025-10-27 18:51:26 +0000

    combat system

--- projects/combat_system.py
+++ projects/combat_system.py
@@ -9,7 +9,7 @@ health_potions = 3
 dmg_die_low = 0
 dmg_die_high = 0
 fled == False
-def attacks(dex,ac,str,low,high,hp):
+def attacks(dex,ac,str,low,high,hp, shp):
     global turn
     global damage
     global attack
@@ -22,75 +22,129 @@ def attacks(dex,ac,str,low,high,hp):
             hp -= damage
         elif attack <= ac:
             hit = False
-    elif turn == "2":
-        print("")
+    elif turn == "2" and user_class != "Mage":
+        attack = dex+random.randint(1,21)
+        if attack > ac:
+            hit = True
+            damage = (str+random.randint(low,high))*2
+            hp -= damage
+            shp -= damage/4
     else:
         print("")
 
 def mon_turn():
-    print("")
+    global turn
+    global mon_dex_mod
+    print("The dire wolf is attacking")
+    turn == random.randint(1,3)
+    def attacks(mon_dex_mod,defense,mon_str_mod,mon_dmg_die_low,mon_dmg_die_high,mon_health)
+    if hit == True:
+        print("")
 def user_turn():
     print("What do you want to do?")
     global turn
-    if user_class == "Fighter":
-        turn = input(f"1. Normal Attack\n 2. Wild Attack(hurts self for 1/4 of damage done for double damage)\n 3. Drink Health Potion({health_potions} left)\n 4. Flee")
-        attacks(dex_mod,mon_defense,str_mod,dmg_die_low,dmg_die_high,mon_health)
-        if hit == True:
-            print(f"You hit for {damage}, the dire wolf now has {mon_health}")
-        elif hit == False:
-            print("You missed")
-        if turn == 3 and health_potions > 0:
-            health += 12
-            health_potions -= 1
-        elif turn == 3 and health_potions <= 0:
-            print("Your out of health potions, you use up your turn looking for something you don't have")
-        if turn == "4":
-            flee = dex_mod+random.randint(1,21)
-            if flee > 16:
-                global fled
-                print("You fled")
-                fled = True
-            elif flee <= 15:
-                print("You failed to flee and used up your turn")
-    elif user_class == "Mage":
-        turn = input(f"1. Normal Attack\n 2. Fireball\n 3. Drink Health Potion({health_potions} left)\n 4. Flee")
-        attacks(dex_mod,mon_defense,str_mod,dmg_die_low,dmg_die_high,mon_health)
-        if hit == True:
-            print(f"You hit for {damage}, the dire wolf now has {mon_health}")
-        elif hit == False:
-            print("You missed")
-        if turn == 2:
-            print("You cast Fireball")
-            if mon_dex_mod+random.randint(1,21) > 16:
-                damage = random.randint(1,7)+random.randint(1,7)+random.randint(1,7)
-                print("The monster succecefully made a saving throw and took half damage")
-        if turn == 3 and health_potions > 0:
-            health += 12
-            health_potions -= 1
-        elif turn == 3 and health_potions <= 0:
-            print("Your out of health potions, you use up your turn looking for something you don't have")
+    while True:
+        if user_class == "Fighter":
+            turn = input(f"1. Normal Attack\n 2. Wild Attack(hurts self for 1/4 of damage done for double damage)\n 3. Drink Health Potion({health_potions} left)\n 4. Flee")
+            attacks(dex_mod,mon_defense,str_mod,dmg_die_low,dmg_die_high,mon_health,health)
+            if hit == True and turn == "1":
+                print(f"You hit for {damage}, the dire wolf now has {mon_health}")
+                break
+            elif hit == False and turn == "1":
+               print("You missed")
+               break
+            if hit == True and turn == "2":
+                print(f"You hit your wild attack for {damage}, and did {damage/4} to yourself. The monster now has {mon_health}, and you have {health}")
+                break
+            elif hit == False and turn == "2":
+                print("You missed your wild attack")
+                break
+            if turn == 3 and health_potions > 0:
+                health += 12
+                health_potions -= 1
+                break
+            elif turn == 3 and health_potions <= 0:
+                print("Your out of health potions, you use up your turn looking for something you don't have")
+                break
+            if turn == "4":
+                flee = dex_mod+random.randint(1,21)
+                if flee > 16:
+                    global fled
+                    print("You fled")
+                    fled = True
+                    break
+                elif flee <= 15:
+                        print("You failed to flee and used up your turn")
+                        break
+            else:
+                print("You inputed something that wasn't an option, try again")
+        elif user_class == "Mage":
+            turn = input(f"1. Normal Attack\n 2. Fireball\n 3. Drink Health Potion({health_potions} left)\n 4. Flee")
+            attacks(dex_mod,mon_defense,str_mod,dmg_die_low,dmg_die_high,mon_health,health)
+            if hit == True:
+                print(f"You hit for {damage}, the dire wolf now has {mon_health}")
+                break
+            elif hit == False:
+                print("You missed")
+                break
+            if turn == 2:
+                print("You cast Fireball")
+                if mon_dex_mod+random.randint(1,21) > 16:
+                    damage = random.randint(1,7)+random.randint(1,7)+random.randint(1,7)
+                    print("The monster succecefully made a saving throw and took half damage, {damage}, and its hp is now {mon_health}")
+            if turn == 3 and health_potions > 0:
+                health += 12
+                health_potions -= 1
+                break
+            elif turn == 3 and health_potions <= 0:
+                print("Your out of health potions, you use up your turn looking for something you don't have")
+                break
+            if turn == "4":
+                flee = dex_mod+random.randint(1,21)
+                if flee > 16:
+                    global fled
+                    print("You fled")
+                    fled = True
+                    break
+                elif flee <= 15:
+                    print("You failed to flee and used up your turn")
+                    break
+            else:
+                print("You inputed something that wasn't an option, try again")        
+        elif user_class == "Rogue":
+            turn = input(f"1. Normal Attack\n 2. Wild Attack(hurts self for 1/4 of damage done for double damage)\n 3. Drink Health Potion({health_potions} left)\n 4. Flee")
+            attacks(dex_mod,mon_defense,str_mod,dmg_die_low,dmg_die_high,mon_health,health)
+            if hit == True:
+                print(f"You hit for {damage}, the dire wolf now has {mon_health}")
+                break
+            elif hit == False:
+                print("You missed")
+                break
+            if hit == True and turn == "2":
+                print(f"You hit your wild attack for {damage}, and did {damage/4} to yourself. The monster now has {mon_health}, and you have {health}")
+                break
+            elif hit == False and turn == "2":
+                print("You missed your wild attack")
+                break
+            if turn == 3 and health_potions > 0:
+                health += 12
+                health_potions -= 1
+                break
+            elif turn == 3 and health_potions <= 0:
+                print("Your out of health potions, you use up your turn looking for something you don't have")
+                break
+            if turn == "4":
+                flee = dex_mod+random.randint(1,21)
+                if flee > 16:
+                    global fled
+                    print("You fled")
+                    fled = True
+                    break
+                elif flee <= 15:
+                    print("You failed to flee and used up your turn")
+                    break
         else:
-            print("")        
-    elif user_class == "Rogue":
-        turn = input(f"1. Normal Attack\n 2. Wild Attack(hurts self for 1/4 of damage done for double damage)\n 3. Drink Health Potion({health_potions} left)\n 4. Flee")
-        attacks(dex_mod,mon_defense,str_mod,dmg_die_low,dmg_die_high,mon_health)
-        if hit == True:
-            print(f"You hit for {damage}, the dire wolf now has {mon_health}")
-        elif hit == False:
-            print("You missed")
-        if turn == 3 and health_potions > 0:
-            health += 12
-            health_potions -= 1
-        elif turn == 3 and health_potions <= 0:
-            print("Your out of health potions, you use up your turn looking for something you don't have")
-        if turn == "4":
-            flee = dex_mod+random.randint(1,21)
-            if flee > 16:
-                global fled
-                print("You fled")
-                fled = True
-            elif flee <= 15:
-                print("You failed to flee and used up your turn")
+            print("You inputed something that wasn't an option, try again")
 user_class_choice = input("What class are you?\n1 Fighter\n 2 Mage\n 3 Rogue\n")
 if user_class_choice == "1":
     health = 30
