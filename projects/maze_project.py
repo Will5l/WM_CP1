a777268b7cf34dc0bfd7515f88549e1dd056981b
Will5l <will.malloch@ucas-edu.net>
2025-11-06 10:53:17 -0700

    maze

--- projects/maze_project.py
+++ projects/maze_project.py
@@ -15,9 +15,78 @@ grid_columnes = [[random.randint(0,1),random.randint(0,1),random.randint(0,1),ra
             [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),],
             [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),]]
 #make function for maze setup
+def mazeSetup():
     #have a turtle draw the outside walls for the maze
+    turtle.penup()
+    turtle.setposition(-300,-300)
+    turtle.forward(100)
+    turtle.pendown()
+    turtle.forward(600)
+    turtle.left(90)
+    turtle.forward(700)
+    turtle.left(90)
+    turtle.penup()
+    turtle.forward(100)
+    turtle.pendown()
+    turtle.forward(600)
+    turtle.left(90)
+    turtle.forward(700)
+    turtle.left(90)
+    turtle.done
+mazeSetup()
     #set it to the first position for the drawing of the inside walls
 #make function for checking if the maze is solvable
     #
 #make function for the actual maze generation
-    #
+def mazeGen():
+    y = 0
+    z = 0
+    x = 0
+    switch = False
+    turtle.setposition(-300,-200)
+    thing = -200
+    while True:
+        if z == 6 and x == 0:
+            thing += 100
+            z = 0
+            y += 1
+            turtle.forward(100)
+            turtle.penup()
+            turtle.setposition(-300,thing)
+        if z == 6 and x == 1:
+            thing += 100
+            z = 0
+            y += 1
+            turtle.forward(100)
+            turtle.penup()
+            turtle.setposition(thing,-300)
+        if y == 6:
+            y = 0
+            z = 0
+            x += 1
+            switch == True
+            thing = -200
+            turtle.left(90)
+            turtle.setposition(-200,-300)
+        if grid_row[y][z] == 1 and x == 0:
+            turtle.pendown()
+            turtle.forward(100)
+            z += 1
+        elif grid_row[y][z] == 0 and x == 0:
+            turtle.penup()
+            turtle.forward(100)
+            z += 1
+        elif x == 0 and switch == True:
+            turtle.setposition(-200,-300)
+            switch == False
+        elif grid_columnes[y][z] == 1 and x == 1:
+            turtle.pendown()
+            turtle.forward(100)
+            z += 1
+        elif grid_columnes[y][z] == 0 and x == 1:
+            turtle.penup()
+            turtle.forward(100)
+            z += 1
+        
+    #have a turtle draw the inside walls after the solvable function based on the lists, where 1 is a wall and pen goes down, and 0 is not a wall, the pen going up
+mazeGen()
