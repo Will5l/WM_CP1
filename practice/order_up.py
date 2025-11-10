5d2aa229e3ee7bc681556c3bcba5bf4fbe9bdce1
Will5l <will.malloch@ucas-edu.net>
2025-11-10 10:51:00 -0700

    order up start

---
+++ practice/order_up.py
@@ -0,0 +1,12 @@
+# WM 2nd Order up
+#Make 3 different dictionaries to store mains, sides, and drinks
+mains = {
+ "Burger": 
+}
+sides = {
+    "Fries": 
+}
+drinks = {
+    "Water": 0.49
+}
+#Have 3 while loops that runs while variables for main the 2 sides and drink are false, and have them be made true when they choose a valid item.
