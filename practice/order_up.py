1ad7d5119b6ae119923916f818df08152383f64e
Will5l <will.malloch@ucas-edu.net>
2025-11-13 10:22:45 -0700

    order up done

--- practice/order_up.py
+++ practice/order_up.py
@@ -4,93 +4,53 @@ main_picked = False
 side1_picked = False
 side2_picked = False
 drink_picked = False
-mains = {
+main = {
  'Burger': 5.99,
  'Steak': 7.99,
  'Sandwich': 6.99,
  'Grilled Cheese': 4.99,
 }
-sides = {
+side = {
     'Steak fries': 1.99,
     'Curly fries': 1.99,
     'Sweet potato fries': 1.99,
     'Tater Tots': 1.99
 }
-drinks = {
+drink = {
     'Water': 1.99,
     'Sprite': 1.99,
     'Pepsi': 1.49,
     'Cream soda': 1.49
 }
-def order(choice, dict, picked, price):
+def order(dict, choosing):
     while True:
-        choice = input("What would you like for your main?" )
+        choice = input(f"What would you like for your {choosing}?" )
         choice = choice.capitalize()
         for key in dict.keys():
             if key == choice:
                 picked = True
         if picked == True:
-            price = mains[main_choice]
+            price = dict[choice]
+            return price, choice
             break
+        else:
+            print("Invailid choice")
 #Print the menu and prices
 print("Mains")
-for key in mains.keys():
-    print(f"{key} : {mains[key]}")
+for key in main.keys():
+    print(f"{key} : {main[key]}")
 print("Sides")
-for key in sides.keys():
-    print(f"{key} : {sides[key]}")
+for key in side.keys():
+    print(f"{key} : {side[key]}")
 print("Drinks")
-for key in drinks.keys():
-    print(f"{key} : {drinks[key]}")
-#Have 3 while loops that runs while variables for main the 2 sides and drink are false, and have them be made true when they choose a valid item.
-while True:
+for key in drink.keys():
+    print(f"{key} : {drink[key]}")
+#Have a function that runs while variables for main the 2 sides and drink are false, and have them be made true when they choose a valid item.
     #Have a variable that stores their choice and have a for loop check if its vaild, if it is, save its price, and break out of the loop.
-    main_choice = input("What would you like for your main?" )
-    main_choice = main_choice.capitalize()
-    for key in mains.keys():
-        if key == main_choice:
-            main_picked = True
-    if main_picked == True:
-        main_price = mains[main_choice]
-        break
-    else:
-        print("Invalid choice")
-while True:
-    #Repeat previous step
-    side1_choice = input("What would you like for your first side?" )
-    side1_choice = side1_choice.capitalize()
-    for key in sides.keys():
-        if key == side1_choice:
-            side1_picked = True
-    if side1_picked == True:
-        side1_price = sides[side1_choice]
-        break
-    else:
-        print("Invalid choice")
-while True:
-    #Repeat previous step
-    side2_choice = input("What would you like for your second side?" )
-    side2_choice = side2_choice.capitalize()
-    for key in sides.keys():
-        if key == side2_choice:
-            side2_picked = True
-    if side2_picked == True:
-        side2_price = sides[side2_choice]
-        break
-    else:
-        print("Invalid choice")
-while True:
-    #Repeat previous step
-    drink_choice = input("What would you like to drink?" )
-    drink_choice = drink_choice.capitalize()
-    for key in drinks.keys():
-        if key == drink_choice:
-            drink_picked = True
-    if drink_picked == True:
-        drink_price = drinks[drink_choice]
-        break
-    else:
-        print("Invalid choice")
+main_price, main_choice = order(main, "main")
+side1_price, side1_choice = order(side, "first side")
+side2_price, side2_choice = order(side, "second side")
+drink_price, drink_choice = order(drink, "drink")
 meal_cost = main_price+side1_price+side2_price+drink_price
 #Have them choose a tip percent and calculate it.
 tip = (int(input("What percent would you like to tip? ")))/100
