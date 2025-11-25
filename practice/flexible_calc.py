91d6e17b45ad36e7a01cad2a9d8b20bad3e5b668
Will5l <will.malloch@ucas-edu.net>
2025-11-25 10:51:51 -0700

    start on flex calc

---
+++ practice/flexible_calc.py
@@ -0,0 +1,16 @@
+# WM 2nd period Flexible Calculator
+
+# I will set up a seperate function for all the equations that will use *args so it can take any amount of numbers
+def sum():
+    total = 0
+    pass
+def average():
+    pass
+def min():
+    pass
+def max():
+    pass
+def product():
+    pass
+#The code will have to have some conditionals to check which operation was used, and then use the appropriate function.
+#The whole thing needs to be in a loop that will break if they say no and continue if they say yes.
